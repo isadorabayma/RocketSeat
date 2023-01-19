0372de03d6fb120696d96079eedc40dc85c132e2
isadora <isadora.bayma@gmail.com>
2023-01-18 21:07:32 -0300

    fisrt route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,17 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient} from '@prisma/client'
 
 const app = fastify()
 const prisma = new PrismaClient()
+
+app.register(cors)
+
+// SELECIONA quais endereços teriam acesso ao back end ao inves de liberar para todos
+
+// app.register(cors, {
+//   origin: ['http.//localhost:3000']
+// })
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany()
