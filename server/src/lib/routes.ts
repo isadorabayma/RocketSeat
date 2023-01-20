27ae4504e2a593a47a1b02fa83a0363b380807a4
isadora <isadora.bayma@gmail.com>
2023-01-20 14:08:50 -0300

    backend - files isolation correction

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -4,27 +4,22 @@ import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 
-export function appRoutes(app: FastifyInstance) {
+export async function appRoutes(app: FastifyInstance) {
   app.post('/habits', async (request) => {
     const createHabitBody = z.object({
-      // title: z.string().nullable -----> para não obrigatóriass 
       title: z.string(),
       weekDays: z.array(
         z.number().min(0).max(6)
       )
     })
-    // const { title, weekDays } = request.body -----> sem o zod
     const { title, weekDays } = createHabitBody.parse(request.body)
     
     const today = dayjs().startOf('day').toDate()
-    // const today = dayjs().startOf('day').toDate()
     
     await prisma.habit.create({
       data: {
         title,
-        // created_at: new Date(), ---> cadastra o dia e as horas
         created_at: today,
-        // v cria os dias da semana na tabela weekDays ao mesmo tempo
         weekDays: {
           create: weekDays.map(weekDay => {
             return {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { appRoutes } from './lib/routes'
 
 const app = fastify()
 
 app.register(cors)
-
-// SELECIONA quais endereços teriam acesso ao back end ao inves de liberar para todos
-// app.register(cors, {
-//   origin: ['http.//localhost:3000']
-// })
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
