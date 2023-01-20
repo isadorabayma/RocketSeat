61b739703f31f7430018bd8e821705d79e45444e
isadora <isadora.bayma@gmail.com>
2023-01-20 07:00:27 -0300

    backend - files isolation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,7 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient} from '@prisma/client'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 
@@ -11,19 +9,6 @@ app.register(cors)
 // app.register(cors, {
 //   origin: ['http.//localhost:3000']
 // })
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-  // ({
-  //   where: {
-  //     title: {
-  //       startsWith: 'beber'
-  //     }
-  //   }
-  // })
-
-  return habits
-})
 
 app.listen({
   port: 3333,
