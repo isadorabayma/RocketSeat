f3921df0bbacb69c0cdf329d3791090b654f85e4
isadora <isadora.bayma@gmail.com>
2023-01-20 15:16:11 -0300

    backend - files isolation correction

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -30,4 +30,48 @@ export async function appRoutes(app: FastifyInstance) {
       }
     })
   })
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      // date: z.date() ----> na verdade o valor vai vir em string e a vai ser necesário converter para o tipo date por isso é usada a função do zod abaixo
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where:{
+        date: parsedDate.toDate(),
+      },
+      include:{
+        dayHabits: true,
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
 }
+
