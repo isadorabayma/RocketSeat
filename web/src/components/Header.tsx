410b3d42bd14d6787906ffa29e687cdcb0445639
isadora <isadora.bayma@gmail.com>
2023-01-22 09:27:37 -0300

    frontend - new habit form

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Plus, X } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog';
-import { useState } from 'react';
+import { NewHabitForm } from './NewHabitForm';
+// import { useState } from 'react';
 
 import logoImage from '../assets/logo.svg'
 
@@ -8,8 +9,9 @@ export function Header() {
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={ logoImage } alt="Logo Habits" />
+      
       <Dialog.Root>
-        {/* o trigger é o butão do radix */}
+        {/* o trigger é o butão do radix que abre o modal*/}
         <Dialog.Trigger
           type="button"
           className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
@@ -17,15 +19,23 @@ export function Header() {
           <Plus size={20} className="text-violet-500 "/>
           New Habit
         </Dialog.Trigger>
+
         {/* o Portal coloca o modal na tela toda lá no index e for do header */}
         <Dialog.Portal>
           <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0"/>
+
           <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+           
             <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
               <X size={24} aria-label="fechar" />
-
             </Dialog.Close>
-            conteudo do modal
+
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm />
+
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
