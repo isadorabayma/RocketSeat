dde5e1499da183c07a965ba53c381411e5f46e05
isadora <isadora.bayma@gmail.com>
2023-01-24 18:37:46 -0300

    frontend - connecting with db and summary route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
+import './lib/dajs'
 
 // import { Habit } from "./components/Habit"
   
