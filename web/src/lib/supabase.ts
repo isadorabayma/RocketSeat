fa0176e1f2179f0327986830d185ad2b19415bc4
isadora <isadora.bayma@gmail.com>
2023-02-02 12:53:25 -0300

    backend - deploy supabase conextion

---
+++ web/src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js"
+
+// export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrbGJpcmlpZWdrcnNneGhrdGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUwODk5ODAsImV4cCI6MTk5MDY2NTk4MH0.3U7jAxNtNnQj8CQ2Qs4MfFWGVNOK0vFwToHoBZUQi2o'
+// export const SUPABASE_URL = 'https://bklbiriiegkrsgxhktcw.supabase.co'
+// export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
